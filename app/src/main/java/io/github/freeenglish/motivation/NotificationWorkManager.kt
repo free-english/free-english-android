e635bbaa15345ce332200494b15831989fac78b2
Sergey Savitsky <sergeysavitsky22@gmail.com>
2019-11-23 00:14:59 +0300

    Merge branch 'master' of https://github.com/free-english/free-english-android
    # Conflicts:
    #	app/src/main/java/io/github/freeenglish/MainActivity.kt
    #	app/src/main/java/io/github/freeenglish/data/sync/DataSyncDao.kt

--- app/src/main/java/io/github/freeenglish/motivation/NotificationWorkManager.kt
+++ app/src/main/java/io/github/freeenglish/motivation/NotificationWorkManager.kt
@@ -42,7 +42,7 @@ class NotificationWorkManager(
 //        Log.d("doWorkRun", "doworkcalled")
         val builder =
             NotificationCompat.Builder(applicationContext, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.ic_notification_icon)
                 .setContentTitle("Motivation title")
                 .setContentText("Some motivation text")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
