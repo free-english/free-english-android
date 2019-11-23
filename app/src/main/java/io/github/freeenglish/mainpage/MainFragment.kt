39bfa81bf13aa7cb953fd9f299c19ef6e8e33cf4
Sergey Savitsky <sergeysavitsky22@gmail.com>
2019-11-23 05:57:10 +0300

    DB queries have been added

--- app/src/main/java/io/github/freeenglish/mainpage/MainFragment.kt
+++ app/src/main/java/io/github/freeenglish/mainpage/MainFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import io.github.freeenglish.R
+import io.github.freeenglish.data.AppDatabase
 import io.github.freeenglish.questions.QuestionFragment
 import kotlinx.android.synthetic.main.main_fragment.*
 
@@ -14,6 +16,8 @@ class MainFragment : Fragment() {
     companion object {
         fun newInstance() = MainFragment()
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,10 +28,14 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        lifecycleScope.launchWhenCreated {
+
+        }
         startTestButton.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.container, QuestionFragment.newInstance())
                 .commit()
+
         }
     }
 }
