550c6ff431214f00de3979d754f9e8b32aeb2c61
Diana <diana-lobach@yandex.ru>
2019-11-23 10:31:49 +0300

    The colors of the answer result have been updated

--- app/src/main/java/io/github/freeenglish/questions/QuestionFragment.kt
+++ app/src/main/java/io/github/freeenglish/questions/QuestionFragment.kt
@@ -66,6 +66,9 @@ class QuestionFragment : Fragment() {
     private fun showCorrectAnswer(resultState: ScreenState.CorrectAnswer) {
         congrat.text = getString(R.string.congrats)
         congrat.setTextColor(ContextCompat.getColor(context!!, R.color.success))
+        rightText.setTextColor(ContextCompat.getColor(context!!, R.color.success))
+        nextButton.setTextColor(ContextCompat.getColor(context!!, R.color.success))
+
         showAnswer(resultState.word, resultState.meaning, resultState.examples, resultState.countAll)
         answer_right.setImageDrawable(
             ContextCompat.getDrawable(
@@ -78,6 +81,9 @@ class QuestionFragment : Fragment() {
     private fun showWrongAnswer(resultState: ScreenState.WrongAnswer) {
         congrat.text = getString(R.string.almost)
         congrat.setTextColor(ContextCompat.getColor(context!!, R.color.error))
+        rightText.setTextColor(ContextCompat.getColor(context!!, R.color.error))
+        nextButton.setTextColor(ContextCompat.getColor(context!!, R.color.error))
+
         showAnswer(resultState.word, resultState.meaning, resultState.examples, resultState.countAll)
         answer_right.setImageDrawable(
             ContextCompat.getDrawable(
